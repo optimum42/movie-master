416e97f40c59a044b160f3f055089377e27b7039
optimum42 <holgermassek@gmail.com>
2026-06-05 22:36:37 +0200

    404 Page added + minor bugfixes

--- app.py
+++ app.py
@@ -220,6 +220,11 @@ def delete_user(user_id):
     return redirect(url_for('index'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     # Only run once on the empty database
 #    with app.app_context():
