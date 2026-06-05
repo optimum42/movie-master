0736c62042cb1540cd0aef0353d0fefacdde9f40
optimum42 <holgermassek@gmail.com>
2026-06-05 21:05:07 +0200

    Delete User and hidden feature for seeding the database added

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, url_for, flash
+from models import db
 from data_manager import DataManager
-from models import db, User
 
 load_dotenv()
 app = Flask(__name__)
@@ -18,53 +18,97 @@ db.init_app(app)  # Link the database and the app.
 data_manager = DataManager()
 
 
+### Flask Routes
 @app.route('/')
 def index():
     """ Renders the home page that shows all users """
+    users = []
     try:
         users = data_manager.get_users()
-        return render_template('index.html', users=users)
 
     except Exception as e:
         db.session.rollback()
         flash(f"Error getting users: {str(e)}", "error")
 
+    return render_template('index.html', users=users)
+
 
 @app.route('/users/<int:user_id>/movies')
-def get_movies(user_id):
+def show_movies(user_id):
     """ Renders the movies page that shows all movies for the given user """
     try:
         # get command line params
         search_query = request.args.get('search')
         sort_by = request.args.get('sort')
 
-        user = data_manager.get_user_by_id(user_id)
+        user = data_manager.get_user(user_id)
         movies = data_manager.get_movies(user_id, search_query, sort_by)
+
         return render_template('movies.html',
                                movies=movies, user=user)
+
     except Exception as e:
         db.session.rollback()
         flash(f"Error getting movies: {str(e)}", "error")
+
+        return redirect(url_for('index'))
 
 
 @app.route('/users/<int:user_id>/movies', methods=['POST'])
 def add_movie(user_id):
     """ Adds a movie to the database """
-    try:
-        title = request.form.get('title')
 
-        if data_manager.add_movie(user_id, title) == True:
-            flash(f"Movie '{title}' successfully added!",
-                  "success")
-        else:
-            flash(f"Movie '{title}' not found or already exist!",
+    title = request.form.get('title')
+    if title.startswith('***'):
+        # hidden feature ***n that seeds the library with n random movies
+        try:
+            add_random_movies(user_id, int(title[3:]))
+        except ValueError:
+            flash("To randomly add movies type '***X' where X is a number between 1 and 100.",
                   "error")
+    else:
+        try:
+            # normal add_movie flow
+            if data_manager.add_movie(user_id, title):
+                flash(f"Movie '{title}' successfully added!",
+                      "success")
+            else:
+                flash(f"Movie '{title}' not found or already exist!",
+                      "error")
 
-        return redirect(url_for('get_movies', user_id=user_id))
+        except Exception as e:
+            db.session.rollback()
+            flash(f"Error adding the movie: {str(e)}", "error")
+
+    return redirect(url_for('show_movies', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/movies/random/<int:movie_number>')
+def add_random_movies(user_id, movie_number):
+    """ Adds n random movies to the database for the given user """
+    try:
+        if movie_number < 1 or movie_number > 100:
+            flash("To randomly add movies type '***X' where X is a number between 1 and 100.",
+                  "error")
+        else:
+            movies_added = data_manager.add_random_movies(user_id, movie_number)
+            if movies_added > 0:
+                msg = f"{movies_added} Random movies added successfully!" \
+                    if movies_added > 1 else "1 Random movie added successfully!"
+                flash(msg,"success")
+            else:
+                flash("No movies were added.",
+                      "error")
+
+    except ValueError:
+        flash("To randomly add movies type '***X' where X is a number between 1 and 100.",
+              "error")
 
     except Exception as e:
         db.session.rollback()
         flash(f"Error adding the movie: {str(e)}", "error")
+
+    return redirect(url_for('show_movies', user_id=user_id))
 
 
 @app.route('/users', methods=['POST'])
@@ -72,23 +116,19 @@ def add_user():
     """ Adds a user to the database """
     try:
         user_name = request.form.get('name')
-        existing_user = data_manager.get_user_by_name(user_name)
-        if existing_user:
-            # Add "error" as a category
+        if data_manager.user_exists(user_name):
             flash(f"Error: A user with the name '{user_name}' "
                   f"already exists!","error")
         else:
             data_manager.create_user(user_name)
-            # Add "success" as a category
             flash(f"User '{user_name}' was added successfully.",
                   "success")
-
-        return redirect(url_for('index'))
 
     except Exception as e:
         db.session.rollback()
         flash(f"Error adding the user: {str(e)}", "error")
 
+    return redirect(url_for('index'))
 
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/delete',
@@ -96,20 +136,17 @@ def add_user():
 def delete_movie(user_id, movie_id):
     """ Deletes a movie link from the users movie list """
     try:
-        # The DataManager handles the entire database logic
-        # (Deleting the link and cleaning up orphaned movies if necessary)
+        title = data_manager.get_movie(movie_id, "title")
         data_manager.delete_movie(user_id, movie_id)
-
-        flash("The movie was successfully removed from the list.",
+        flash(f"The movie '{title}' was successfully removed from the list.",
               "success")
-
-        # After deletion, we redirect the user back to their movie list
-        return redirect(url_for('get_movies', user_id=user_id))
 
     except Exception as e:
         # In case a database error occurs (e.g., connection issues)
         db.session.rollback()
         flash(f"Error deleting the movie: {str(e)}", "error")
+
+    return redirect(url_for('show_movies', user_id=user_id))
 
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/update')
@@ -118,32 +155,32 @@ def show_update_form(user_id, movie_id):
 
     try:
         movie = data_manager.get_movie(movie_id)
-        link = data_manager.get_user_movie_link(movie_id, user_id)
+        rating = data_manager.get_movie_rating(movie_id, user_id)
 
         return render_template('update_movie.html',
-                           movie=movie, user_id=user_id, rating=link.rating)
+                           movie=movie, user_id=user_id, rating=rating)
 
     except Exception as e:
         # In case a database error occurs (e.g., connection issues)
         db.session.rollback()
         flash(f"Error getting movie info: {str(e)}", "error")
 
+    return redirect(url_for('show_movies', user_id=user_id))
+
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/update',
            methods=['POST'])
 def update_movie(user_id, movie_id):
-    """ Updates a movie link from the users movie list """
+    """ Updates a movie rating from the users movie list """
 
     try:
         new_rating = request.form.get('rating')
         rating_val = float(new_rating) if new_rating else None
         data_manager.update_movie(movie_id, user_id, rating_val)
 
-        movie = data_manager.get_movie(movie_id)
-        flash(
-            f"The rating for '{movie.title}' was updated successfully!",
-            "success")
-        return redirect(url_for('get_movies', user_id=user_id))
+        title = data_manager.get_movie(movie_id, "title")
+        flash(f"The rating for '{title}' was updated successfully!",
+              "success")
 
     except ValueError:
         flash("Invalid rating. Please enter a valid number.",
@@ -153,11 +190,40 @@ def update_movie(user_id, movie_id):
         db.session.rollback()
         flash(f"Error updating the movie: {str(e)}", "error")
 
+    return redirect(url_for('show_movies', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/delete')
+def authorize_delete_user(user_id):
+    """ Authorizes the user to delete their account """
+    return render_template('password.html', user_id=user_id,
+                           callback=url_for('delete_user', user_id=user_id))
+
+
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    """ Deletes a user from the database """
+    try:
+        password = request.form.get('password')
+        if password != os.getenv("ADMIN_PASSWORD"):
+            flash("Incorrect password.", "error")
+            return redirect(url_for('authorize_delete_user', user_id=user_id))
+
+        data_manager.delete_user(user_id)
+        flash("User successfully removed.",
+              "success")
+
+    except Exception as e:
+        db.session.rollback()
+        flash(f"Error deleting the user: {str(e)}", "error")
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    # Only run once on empty database
+    # Only run once on the empty database
 #    with app.app_context():
 #       db.drop_all()
 #       db.create_all()
 
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True , host='0.0.0.0', port=5001)
--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,22 @@
 from models import db, User, Movie, UserMovies
-from api.omdb_api import fetch_movie, get_imdb_url
+from api import omdb_api
+
+import random
+from data import blockbusters
 
 
-class DataManager():
+class DataManager:
     """
-    Handles database operations for all three tables
+    Handles the database operations
     """
+
+    def user_exists(self, user_name):
+        """ checks if the user exists in the database already """
+        user = User.query.filter_by(name=user_name).first()
+        return user is not None
+
+    def get_user(self, user_id):
+        return User.query.get(user_id)
 
     def create_user(self, name):
         new_user = User(name=name)
@@ -15,13 +26,7 @@ class DataManager():
     def get_users(self):
         return User.query.all()
 
-    def get_user_by_id(self, user_id):
-        return User.query.get(user_id)
-
-    def get_user_by_name(self, user_name):
-        return User.query.filter_by(name=user_name).first()
-
-    def get_movies(self, user_id, search_query, sort_by):
+    def get_movies(self, user_id, search_query=None, sort_by=None):
         """ Returns all user movies that match the search query """
 
         # Join Movies with the UserMovies table and filter by user_id
@@ -29,7 +34,7 @@ class DataManager():
              .join(UserMovies, Movie.id == UserMovies.movie_id) \
              .filter(UserMovies.user_id == user_id)
 
-        # filter by title if search query is not empty
+        # filter by title if the search query is not empty
         if search_query:
             query = query.filter(Movie.title.like("%" + search_query + "%"))
 
@@ -48,26 +53,37 @@ class DataManager():
         #
         # return result
 
-    def get_movie(self, movie_id):
+    def get_movie(self, movie_id, column=""):
+        """
+        Returns the value of the specified tabel column if it exists,
+        else returns the whole movie object
+        """
         movie = Movie.query.get(movie_id)
-        return movie
+        if column in movie.__table__.columns:
+            return movie.__getattribute__(column)
+        else:
+            return movie
 
-    def get_user_movie_link(self, movie_id, user_id):
+    def get_movie_rating(self, movie_id, user_id):
+        """ Returns the rating of a movie for a user """
         link = UserMovies.query.filter_by(user_id=user_id, movie_id=movie_id).first()
-        return link
+        if link is None:
+            return None
+        return link.rating
 
     def add_movie(self, user_id, title):
-        # check that user exists
-        user = self.get_user_by_id(user_id)
+        """ Adds a movie to a user's list """
+        # check that the user exists
+        user = User.query.get(user_id)
         if not user:
-            raise ValueError("User does not exist.")
+            raise Exception(f"User ID '{user_id}' does not exist.")
 
-        # first fetch API information to get the correct title and the rating
-        data = fetch_movie(title)
+        # fetch API information to get the correct title and the rating
+        data = omdb_api.fetch_movie(title)
         if data is None:
             return False
 
-        title = data.get("Title", title)
+        title = data.get("Title", title) or title
         rating = data.get("imdbRating", 0)
 
         # check if the movie already exists globally in the database
@@ -80,17 +96,16 @@ class DataManager():
                 year=int(data.get("Year"))
                     if data.get("Year", "").isdigit() else None,
                 poster=data.get("Poster"),
-                imdb_url=get_imdb_url(title)
+                imdb_url=omdb_api.get_imdb_url(title)
             )
             db.session.add(movie)
             db.session.commit()  # Commit so the movie gets an ID (movie.id)!
 
         # check if the user already has the movie in its list
-        link = self.get_user_movie_link(movie.id, user_id)
+        link = UserMovies.query.filter_by(user_id=user_id, movie_id=movie.id).first()
         if not link:
             # create the link between User and Movie
-            link = UserMovies(user_id=user_id, movie_id=movie.id,
-                                  rating=rating)
+            link = UserMovies(user_id=user_id, movie_id=movie.id, rating=rating)
             db.session.add(link)
             db.session.commit()
             return True
@@ -98,34 +113,54 @@ class DataManager():
             return False
 
     def delete_movie(self, user_id, movie_id):
-        """
-        Removes a movie from a user's list.
-        If there is no user left who links to it, remove the movie
-        """
-        # Delete the link between User and Movie
-        link = self.get_user_movie_link(movie_id, user_id)
-        if link:
-            db.session.delete(link)
-            db.session.commit()
+        """ Removes a movie from a user's list """
 
-        # delete the movie if nobody links to it
-        remaining_links = UserMovies.query.filter_by(movie_id=movie_id).count()
-        if remaining_links == 0:
-            movie = Movie.query.get(movie_id)
-            if movie:
-                db.session.delete(movie)
-                db.session.commit()
+        # Delete the link between User and Movie, the movie stays in the database
+        UserMovies.query.filter_by(user_id=user_id, movie_id=movie_id).delete()
+        db.session.commit()
 
-        return True
+
+    def user_movie_exists(self, user_id, movie_id):
+        """ Checks if the user has already added the movie to their list """
+        link = UserMovies.query.filter_by(user_id=user_id, movie_id=movie_id).first()
+        return link is not None
 
     def update_movie(self, movie_id, user_id, rating):
-        """
-        Updates the user based movie ranking
-        """
-        link = self.get_user_movie_link(movie_id, user_id)
-        if not link:
+        """ Updates the user's based movie ranking """
+        if not self.user_movie_exists(user_id, movie_id):
+            return False
+
+        link = UserMovies.query.filter_by(user_id=user_id, movie_id=movie_id).first()
+        if link is None:
             return False
 
         link.rating = rating
         db.session.commit()
+        return True
+
+    def add_random_movies(self, user_id, movie_number):
+        """ Adds random movies to the database """
+        added_number = 0
+        for i in range(movie_number):
+            title = random.choice(blockbusters.TOP_MOVIES)
+            if self.add_movie(user_id, title):
+                added_number += 1
+
+        return added_number
+
+    def delete_user(self, user_id):
+        """ Deletes a user and all its movie links from the database """
+
+        # check if the user exists
+        if self.get_user(user_id) is None:
+            return False
+
+        # Delete all movie links for the user
+        UserMovies.query.filter_by(user_id=user_id).delete()
+        db.session.commit()
+
+        # Delete the user
+        User.query.filter_by(id=user_id).delete()
+        db.session.commit()
+
         return True
